a1f1d81d313b8b8efddf10139c38386caec10c3b
surepeps <gatukurh1@gmail.com>
2025-11-30 21:27:08 +0000

    Add memo to imports in PropertyCard
    cgen-32e850094fa94610b036a9f9da69ded8

--- src/components/new-marketplace/cards/PropertyCard.tsx
+++ src/components/new-marketplace/cards/PropertyCard.tsx
@@ -1,7 +1,7 @@
 /** @format */
 
 "use client";
-import React, { useState } from "react";
+import React, { useState, memo } from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
