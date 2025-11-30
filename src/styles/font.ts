f3fe03a3312e0f523c2d166bab98443100af23df
surepeps <gatukurh1@gmail.com>
2025-11-30 21:23:23 +0000

    Optimize font imports - keep only essential fonts
    cgen-731baa162f104188b61505422eed932d

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -1,33 +1,13 @@
 /** @format */
 
-// styles/font.js
-import {
-  Roboto,
-  Open_Sans,
-  Epilogue,
-  Archivo,
-  Ubuntu,
-  PT_Sans,
-  Manrope,
-} from 'next/font/google';
+import { Roboto, Archivo } from 'next/font/google';
 
 export const roboto = Roboto({
   variable: '--font-roboto',
   subsets: ['latin'],
-  weight: ['400', '700'], // Specify weights you want to use (optional)
-});
-
-export const epilogue = Epilogue({
-  subsets: ['latin'], // Specify subsets
-  weight: ['400', '700'], // Specify weights
-  style: ['normal', 'italic'], // Specify styles
-  variable: '--font-epilogue', // Define a custom CSS variable
-});
-
-export const openSans = Open_Sans({
-  subsets: ['latin'],
-  weight: ['400'],
-  variable: '--font-open-sans',
+  weight: ['400', '700'],
+  display: 'swap',
+  preload: true,
 });
 
 export const archivo = Archivo({
@@ -35,25 +15,5 @@ export const archivo = Archivo({
   weight: ['400', '500', '600', '700'],
   variable: '--font-archivo',
   display: 'swap',
-});
-
-export const ubuntu = Ubuntu({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  style: ['normal', 'italic'],
-  variable: '--font-ubuntu',
-});
-
-export const product_sans = PT_Sans({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  style: ['normal', 'italic'],
-  variable: '--font-pt-sans',
-});
-
-export const manrope = Manrope({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  style: ['normal'],
-  variable: '--font-manrope',
+  preload: true,
 });
