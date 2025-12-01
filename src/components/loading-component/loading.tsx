20f41fdd440e1e7024b1a3738ba11a12f1540ff8
surepeps <gatukurh1@gmail.com>
2025-12-01 12:28:13 +0000

    completionId: cgen-3d200e8576224a80b2603f2d42210f44
    cgen-3d200e8576224a80b2603f2d42210f44

--- src/components/loading-component/loading.tsx
+++ src/components/loading-component/loading.tsx
@@ -2,13 +2,12 @@
 
 'use client';
 import React from 'react';
-import 'ldrs/react/Trio.css';
-import { Trio } from 'ldrs/react';
+// import 'ldrs/react/Trio.css';
+// import { Trio } from 'ldrs/react';
 
 const Loading = () => {
   return (
     <section className='h-screen w-full flex items-center justify-center'>
-      <Trio size={50} speed={1.3} color='#09391C' />
     </section>
   );
 };
