d7d33f1b2e1a6a6d65f3e5a1d8c7a19057613fda
surepeps <gatukurh1@gmail.com>
2025-12-01 13:12:04 +0000

    completionId: cgen-8de3b92274ef49469a4386d36ae9a0e1
    cgen-8de3b92274ef49469a4386d36ae9a0e1

--- src/components/loading-component/loading.tsx
+++ src/components/loading-component/loading.tsx
@@ -1,12 +1,16 @@
 /** @format */
-
 'use client';
-import React from 'react';
+
+import { motion } from "framer-motion";
 
 const Loading = () => {
   return (
-    <section className='h-screen w-full flex items-center justify-center'>
-      <div>Loading...</div>
+    <section className="h-screen w-full flex items-center justify-center">
+      <motion.div
+        className="w-12 h-12 rounded-full border-4 border-[#09391C] border-t-transparent"
+        animate={{ rotate: 360 }}
+        transition={{ repeat: Infinity, duration: 0.8, ease: "linear" }}
+      />
     </section>
   );
 };
