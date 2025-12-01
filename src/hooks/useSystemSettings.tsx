1061b37f0923b79601a2af80f4c296c654162f89
surepeps <gatukurh1@gmail.com>
2025-12-01 12:40:22 +0000

    completionId: cgen-18a48f39af284814b5a71112ca7accd4
    cgen-18a48f39af284814b5a71112ca7accd4

--- src/hooks/useSystemSettings.tsx
+++ src/hooks/useSystemSettings.tsx
@@ -7,6 +7,18 @@ interface SystemSettings {
   maintenanceMode: boolean;
 }
 
+interface SocialLinksSettings {
+  facebook_url?: string;
+  twitter_url?: string;
+  instagram_url?: string;
+  linkedin_url?: string;
+  youtube_url?: string;
+  tiktok_url?: string;
+  whatsapp_url?: string;
+  telegram_url?: string;
+  website_url?: string;
+}
+
 /**
  * @useSystemSettings - Hook to manage system-wide settings
  * @returns - Object containing settings and loading state
@@ -36,4 +48,20 @@ export const useHomePageSettings = () => {
     loading,
     settings,
   };
+};
+
+/**
+ * @useSocialLinksSettings - Hook to manage social media links settings
+ * @returns - Object containing social links settings
+ */
+export const useSocialLinskSettings = () => {
+  const [settings, setSettings] = useState<SocialLinksSettings>({
+    facebook_url: 'https://facebook.com/khabiteq',
+    twitter_url: 'https://twitter.com/khabiteq',
+    instagram_url: 'https://instagram.com/khabiteq',
+    linkedin_url: 'https://linkedin.com/company/khabiteq',
+    youtube_url: 'https://youtube.com/@khabiteq',
+  });
+
+  return { settings };
 };
