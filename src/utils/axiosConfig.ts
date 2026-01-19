08964c43b4a5c22751268b46d37012039270abaf
surepeps <gatukurh1@gmail.com>
2026-01-19 09:06:36 +0000

    Fix axiosConfig headers any type cast
    cgen-6fad635b3da242d1adce8f781a77f2ee

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -10,10 +10,7 @@ api.interceptors.request.use((config) => {
   try {
     const token = typeof window !== 'undefined' ? Cookies.get('token') : null;
     if (token) {
-      config.headers = {
-        ...(config.headers || {}),
-        Authorization: `Bearer ${token}`,
-      } as any;
+      config.headers.Authorization = `Bearer ${token}`;
     }
   } catch (_) {
     // ignore storage access errors in non-browser contexts
