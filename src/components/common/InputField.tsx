c693d1e10874a110c3e2bf6a1c6bee3af62a5d39
surepeps <gatukurh1@gmail.com>
2026-01-19 09:07:11 +0000

    Fix InputField component generic syntax
    cgen-71c09502bbcb4c7792203394b3cc3a1e

--- src/components/common/InputField.tsx
+++ src/components/common/InputField.tsx
@@ -18,7 +18,7 @@ interface InputFieldProps<T extends Record<string, unknown> = Record<string, unk
   className?: string;
 }
 
-const InputField: FC<InputFieldProps> = <T extends Record<string, unknown> = Record<string, unknown>>({
+const InputField: FC<InputFieldProps> = ({
   label,
   name,
   type = 'text', // Default to 'text'
