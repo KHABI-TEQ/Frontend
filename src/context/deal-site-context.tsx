0d17a219580746382b831081cfd7cf6dc9cc3452
surepeps <gatukurh1@gmail.com>
2026-01-26 16:00:57 +0000

    Add missing properties to InspectionDesignSettings
    cgen-0e27765dae9b4835ba9be1555f222be2

--- src/context/deal-site-context.tsx
+++ src/context/deal-site-context.tsx
@@ -46,6 +46,8 @@ export interface PublicPageDesign {
 
 export interface InspectionDesignSettings {
   defaultInspectionFee: number | "";
+  allowPublicBooking?: boolean;
+  negotiationEnabled?: boolean;
 }
 
 export interface AboutHeroCta {
